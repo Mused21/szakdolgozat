cc6b2a48d94fb3999e15ce439ebd8a0f0639c281
Mused21 <imrenorbert21@gmail.com>
2024-02-19 20:58:14 +0100

    Test framework

--- test_framework.cpp
+++ test_framework.cpp
@@ -1,58 +1,139 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <fstream>
 #include "graphalg.h"
 
 using namespace std;
 
+class DirectedGraphGenerator {
+public:
+    static vector<pair<int, int>> generateGraph(int N, int M) {
+        vector<pair<int, int>> edges;
+        unordered_set<string> existingEdges;
+        srand(time(nullptr));
 
-int main()
-{
-    int N;
-    cout << "How many nodes?" << endl;
+        for (int i = 0; i < M - 1; ++i) {
+            int u, v;
+            do {
+                u = rand() % N;
+                v = rand() % N; 
+            } while (u >= v || existingEdges.find(edgeKey(u, v)) != existingEdges.end());
+
+            edges.emplace_back(u, v);
+            existingEdges.insert(edgeKey(u, v));
+        }
+        // ez igy sokszor eredmenyez aciklikus grafot tbh
+        /*if (M > N - 1) {
+            int u, v;
+            do {
+                u = rand() % (N - 1) + 1;
+                v = rand() % u;
+            } while (existingEdges.find(edgeKey(u, v)) != existingEdges.end());
+
+            edges.emplace_back(u, v);
+            existingEdges.insert(edgeKey(u, v));
+        }
+        */
+        if (M > N - 1 && !edges.empty()) {
+            int randomIndex = rand() % edges.size();
+            auto& selectedEdge = edges[randomIndex];
+        
+            int u = selectedEdge.second;
+            int v = selectedEdge.first;
+
+            edges.emplace_back(u, v);
+            existingEdges.insert(edgeKey(u, v));
+        }
+
+        return edges;
+    }
+
+private:
+    static string edgeKey(int u, int v) {
+        return to_string(u) + "->" + to_string(v);
+    }
+};
+
+
+int main() {   
+    int N, M, C;
+    cout << "Enter the number of nodes (N): ";
     cin >> N;
-
-    int M;
-    cout << "How many edges per node?" << endl;
+    cout << "Enter the total number of edges (M): ";
     cin >> M;
+    cout << "Enter the total number of runs (C): ";
+    cin >> C;
 
-    // ok ezt tudom h nem pontosan annyi mert az utolso M-bol nem fog kiindulni csucs, majd ezen javitok, de elsore jo kozelites
+    int i = 0;
+    long staticSum = 0;
+    long dynamicSum = 0;
 
-    vector<pair<int, int>> totalEdgesToInsert;
+    while (i < C) {
+        bool dynamicFoundCycle = false;
+        pair<int, int> dynamicCycle;
+        bool staticFoundCycle = false;
+        pair<int, int> staticCycle;
 
-    for (size_t i = 0; i < N - M; i++) {
-        for (size_t j = 0; j < M; j++)
-        {
-            totalEdgesToInsert.push_back(make_pair(i, i + j + 1));
+        auto start_generation = chrono::high_resolution_clock::now();
+        auto edges = DirectedGraphGenerator::generateGraph(N, M);
+        auto stop_generation = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::milliseconds>(stop_generation - start_generation);
+
+        cout << "Generation lasted " << duration.count() << " ms." << endl;
+
+        auto start_dynamic = chrono::high_resolution_clock::now();
+        DynamicGraph dg = DynamicGraph(N);
+        for (auto edge : edges) {
+            if (!dg.insertEdge(edge)) {
+                auto stop_dynamic = chrono::high_resolution_clock::now();
+                auto duration = chrono::duration_cast<chrono::milliseconds>(stop_dynamic - start_dynamic);
+                dynamicFoundCycle = true;
+                dynamicCycle = edge;
+                dynamicSum += duration.count();
+                break;
+            }
         }
-    }
-    totalEdgesToInsert.push_back(make_pair(N - 1, N - M - 1));
 
-    auto start_dynamic = chrono::high_resolution_clock::now();
+        auto start_static = chrono::high_resolution_clock::now();
+        StaticGraph sg = StaticGraph(N);
+        for (auto edge : edges) {
+            if (!sg.insertEdge(edge)) {
+                auto stop_static = chrono::high_resolution_clock::now();
+                auto duration = chrono::duration_cast<chrono::milliseconds>(stop_static - start_static);
+                staticFoundCycle = true;
+                staticCycle = edge;
+                staticSum += duration.count();
+                break;
+            }
+        }
 
-    DynamicGraph dg = DynamicGraph(N);
+        if (dynamicFoundCycle && staticFoundCycle) {
+            i++;
+        }
 
-    for (auto edge : totalEdgesToInsert) {
-        if (!dg.insertEdge(edge)) {
-            auto stop_dynamic = chrono::high_resolution_clock::now();
-            auto duration = chrono::duration_cast<chrono::milliseconds>(stop_dynamic - start_dynamic);
-            cout << "Dynamic: Cycle was formed at inserting edge: (" << edge.first << ", " << edge.second << "). Time taken: " << duration.count() << " ms." << endl;
+        if (!dynamicFoundCycle && !staticFoundCycle) {
+            cout << "No cycle found, regenerating graph." << endl;
+            continue;
+        }
+
+        if (!dynamicFoundCycle && staticFoundCycle) {
+            cout << "ERROR: Dynamic graph method did not find the cycle formed at: " << staticCycle.first << "->" << staticCycle.second;
+            break;
+        }
+
+        if (!dynamicFoundCycle && staticFoundCycle) {
+            cout << "ERROR: Static graph method did not find the cycle formed at: " << dynamicCycle.first << "->" << dynamicCycle.second;
             break;
         }
     }
 
-    auto start_static = chrono::high_resolution_clock::now();
-    StaticGraph sg = StaticGraph(N);
+    cout << "Results: " << endl;
+    cout << "Number of nodes: " << N << endl;
+    cout << "Number of edges: " << M << endl;
+    cout << "Number of runs: " << C << endl;
+    cout << "Dynamic graph speed average: " << dynamicSum / C << endl;
+    cout << "Static graph speed average: " << staticSum / C << endl;
 
-    for (auto edge : totalEdgesToInsert) {
-        if (!sg.insertEdge(edge)) {
-            auto stop_static = chrono::high_resolution_clock::now();
-            auto duration = chrono::duration_cast<chrono::milliseconds>(stop_static - start_static);
-            cout << "Static: Cycle was formed at inserting edge: (" << edge.first << ", " << edge.second << "). Time taken: " << duration.count() << " ms." << endl;
-            break;
-        }
-    }
-
-    cout << "Done" << endl;
     return 0;
 }
