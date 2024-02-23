5367e6d0d7617cfb4ca4e376bc47ecb6395b0c2b
Mused21 <imrenorbert21@gmail.com>
2024-02-23 14:14:51 +0100

    Add another graph generation method

--- graphalg.cpp
+++ graphalg.cpp
@@ -1,43 +1,54 @@
 #include "graphalg.h"
 #include <cmath>
 #include <random>
+#include <algorithm>
 #include <queue>
 
 StaticGraph::StaticGraph(int N) : N(N), adj(N), inDegree(N), everSeen(N, false), nodeCounter(0) {}
 
-void StaticGraph::update(const pair<int, int>& edge) {
+void StaticGraph::update(const pair<int, int> &edge)
+{
     adj[edge.first].push_back(edge.second);
     inDegree[edge.second]++;
-    if (inDegree[edge.first] == 0) {
+    if (inDegree[edge.first] == 0)
+    {
         zeroDegree.insert(edge.first);
     }
-    if (inDegree[edge.second] == 1) {
+    if (inDegree[edge.second] == 1)
+    {
         zeroDegree.erase(edge.second);
     }
-    if (!everSeen[edge.first]) {
+    if (!everSeen[edge.first])
+    {
         everSeen[edge.first] = true;
         nodeCounter++;
     }
-    if (!everSeen[edge.second]) {
+    if (!everSeen[edge.second])
+    {
         everSeen[edge.second] = true;
         nodeCounter++;
     }
 }
 
-bool StaticGraph::hasCycle() {
+bool StaticGraph::hasCycle()
+{
     queue<int> q;
-    for (int element : zeroDegree) {
+    for (int element : zeroDegree)
+    {
         q.push(element);
     }
     int count = 0;
     vector<int> inDegreeCopy = inDegree;
 
-    while (!q.empty()) {
+    while (!q.empty())
+    {
         int u = q.front();
         q.pop();
 
-        for (int i = 0; i < adj[u].size(); i++) {
-            if (--inDegreeCopy[adj[u][i]] == 0) {
+        for (int i = 0; i < adj[u].size(); i++)
+        {
+            if (--inDegreeCopy[adj[u][i]] == 0)
+            {
                 q.push(adj[u][i]);
             }
         }
@@ -47,17 +58,19 @@ bool StaticGraph::hasCycle() {
     return count != nodeCounter;
 }
 
-bool StaticGraph::insertEdge(const pair<int, int>& edge) {
+bool StaticGraph::insertEdge(const pair<int, int> &edge)
+{
     update(edge);
     return !hasCycle();
 }
 
-DynamicGraph::DynamicGraph(int N) : N(N) {
+DynamicGraph::DynamicGraph(int N) : N(N)
+{
     SAMPLE_PROBABILITY_THRESHOLD = 11 * log2(N) / sqrt(N);
     random_device rd;
     default_random_engine generator(rd());
     uniform_real_distribution<double> distribution(0.0, 1.0);
-    
+
     A.resize(N);
     D.resize(N);
     As.resize(N);
@@ -65,8 +78,10 @@ DynamicGraph::DynamicGraph(int N) : N(N) {
     AA.resize(N);
     edges.resize(N);
 
-    for (int i = 0; i < N; ++i) {
-        if (distribution(generator) <= SAMPLE_PROBABILITY_THRESHOLD) {
+    for (int i = 0; i < N; ++i)
+    {
+        if (distribution(generator) <= SAMPLE_PROBABILITY_THRESHOLD)
+        {
             S.insert(i);
             As[i].insert(i);
             Ds[i].insert(i);
@@ -77,51 +92,67 @@ DynamicGraph::DynamicGraph(int N) : N(N) {
     }
 }
 
-void DynamicGraph::update(const pair<int, int>& edge) {
+void DynamicGraph::update(const pair<int, int> &edge)
+{
     int u = edge.first;
     int v = edge.second;
 
     edges[u].insert(v);
 
-    for (auto desc : D[v]) {
+    for (auto desc : D[v])
+    {
         A[desc].insert(A[u].begin(), A[u].end());
-        if (S.count(desc)) {
-            for (auto anc : A[u]) {
+        if (S.count(desc))
+        {
+            for (auto anc : A[u])
+            {
                 Ds[anc].insert(desc);
             }
         }
     }
 
-    for (auto anc : A[u]) {
+    for (auto anc : A[u])
+    {
         D[anc].insert(D[v].begin(), D[v].end());
-        if (S.count(anc)) {
-            for (auto desc : D[v]) {
+        if (S.count(anc))
+        {
+            for (auto desc : D[v])
+            {
                 As[desc].insert(anc);
             }
         }
     }
 }
 
-bool DynamicGraph::isSEquivalent(int u, int v) {
+bool DynamicGraph::isSEquivalent(int u, int v)
+{
     return (As[u].size() == As[v].size()) && (Ds[u].size() == Ds[v].size());
 }
 
-bool DynamicGraph::hasCycle(const pair<int, int>& edge) {
+bool DynamicGraph::hasCycle(const pair<int, int> &edge)
+{
     int u = edge.first;
     int v = edge.second;
     queue<int> to_explore;
     to_explore.push(v);
 
-    while (!to_explore.empty()) {
+    while (!to_explore.empty())
+    {
         int w = to_explore.front();
         to_explore.pop();
-        if ((w == u) || (AA[u].count(w))) {
+        if ((w == u) || (AA[u].count(w)))
+        {
             return true;
-        } else if (AA[w].count(u) || !isSEquivalent(u, w)) {
+        }
+        else if (AA[w].count(u) || !isSEquivalent(u, w))
+        {
             continue;
-        } else {
+        }
+        else
+        {
             AA[w].insert(u);
-            for (const auto& e : edges[w]) {
+            for (const auto &e : edges[w])
+            {
                 to_explore.push(e);
             }
         }
@@ -129,7 +160,8 @@ bool DynamicGraph::hasCycle(const pair<int, int>& edge) {
     return false;
 }
 
-bool DynamicGraph::insertEdge(const pair<int, int>& edge) {
+bool DynamicGraph::insertEdge(const pair<int, int> &edge)
+{
     update(edge);
     return !hasCycle(edge);
 }
--- test_framework.cpp
+++ test_framework.cpp
@@ -9,81 +9,111 @@
 
 using namespace std;
 
-class DirectedGraphGenerator {
+class DirectedGraphGenerator
+{
 public:
-    static vector<pair<int, int>> generateGraph(int N, int M) {
+    static vector<pair<int, int>> generateGraph(int N, int M, int Gver)
+    {
         vector<pair<int, int>> edges;
-        unordered_set<string> existingEdges;
-        srand(time(nullptr));
 
-        vector<int> array(N);
-        iota(array.begin(), array.end(), 0);
-        mt19937 rng(random_device{}());
-        uniform_int_distribution<> dist(0, N - 1);
-        std::shuffle(array.begin(), array.end(), rng);
+        if (Gver == 1)
+        {
+            unordered_set<string> existingEdges;
+            srand(time(nullptr));
 
-        for (int i = 0; i < M; ++i) {
-            int u, v;
-            string key;
-            do {
-                u = dist(rng);
-                v = dist(rng);
-                if (u > v) {
-                    swap(u, v);
-                }
-                key = edgeKey(array[u], array[v]);
-            } while (u == v || existingEdges.find(key) != existingEdges.end());
+            vector<int> array(N);
+            iota(array.begin(), array.end(), 0);
+            mt19937 rng(random_device{}());
+            uniform_int_distribution<> dist(0, N - 1);
+            shuffle(array.begin(), array.end(), rng);
 
-            edges.emplace_back(array[u], array[v]);
-            existingEdges.insert(key);
+            for (int i = 0; i < M; ++i)
+            {
+                int u, v;
+                string key;
+                do
+                {
+                    u = dist(rng);
+                    v = dist(rng);
+                    if (u > v)
+                    {
+                        swap(u, v);
+                    }
+                    key = edgeKey(array[u], array[v]);
+                } while (u == v || existingEdges.find(key) != existingEdges.end());
+
+                edges.emplace_back(array[u], array[v]);
+                existingEdges.insert(key);
+            }
+
+            for (int i = 0; i < M / 100; ++i)
+            {
+                int u, v;
+                string key;
+                do
+                {
+                    u = dist(rng);
+                    v = dist(rng);
+                    if (u < v)
+                    {
+                        swap(u, v);
+                    }
+                    key = edgeKey(array[u], array[v]);
+                } while (u == v || existingEdges.find(key) != existingEdges.end());
+
+                edges.emplace_back(array[u], array[v]);
+                existingEdges.insert(key);
+            }
         }
-
-        for (int i = 0; i < M/100; ++i) {
-            int u, v;
-            string key;
-            do {
-                u = dist(rng);
-                v = dist(rng);
-                if (u < v) {
-                    swap(u, v);
+        else
+        {
+            for (size_t i = 0; i < N; i++)
+            {
+                for (size_t j = 1; j <= M / N; j++)
+                {
+                    if (i + j < N)
+                    {
+                        edges.emplace_back(i, i + j);
+                    }
                 }
-                key = edgeKey(array[u], array[v]);
-            } while (u == v || existingEdges.find(key) != existingEdges.end());
-
-            edges.emplace_back(array[u], array[v]);
-            existingEdges.insert(key);
+            }
+            edges.emplace_back(N - 1, 0);
         }
         return edges;
     }
 
 private:
-    static string edgeKey(int u, int v) {
+    static string edgeKey(int u, int v)
+    {
         return to_string(u) + "->" + to_string(v);
     }
 };
 
-
-int main() {   
-    int N, M, C;
+int main()
+{
+    int N, M, C, Gver;
     cout << "Enter the number of nodes (N): ";
     cin >> N;
     cout << "Enter the total number of edges (M): ";
     cin >> M;
     cout << "Enter the total number of runs (C): ";
     cin >> C;
+    cout << "Graph generator version (G) [1 - random or 2 - uniform]: ";
+    cin >> Gver;
 
     int i = 0;
     long staticSum = 0;
     long dynamicSum = 0;
 
-    while (i < C) {
+    while (i < C)
+    {
         bool dynamicFoundCycle = false;
         pair<int, int> dynamicCycle;
         bool staticFoundCycle = false;
         pair<int, int> staticCycle;
 
         auto start_generation = chrono::high_resolution_clock::now();
-        auto edges = DirectedGraphGenerator::generateGraph(N, M);
+        auto edges = DirectedGraphGenerator::generateGraph(N, M, Gver);
         auto stop_generation = chrono::high_resolution_clock::now();
         auto duration = chrono::duration_cast<chrono::milliseconds>(stop_generation - start_generation);
 
@@ -91,8 +121,10 @@ int main() {
 
         auto start_dynamic = chrono::high_resolution_clock::now();
         DynamicGraph dg = DynamicGraph(N);
-        for (auto edge : edges) {
-            if (!dg.insertEdge(edge)) {
+        for (auto edge : edges)
+        {
+            if (!dg.insertEdge(edge))
+            {
                 auto stop_dynamic = chrono::high_resolution_clock::now();
                 auto duration = chrono::duration_cast<chrono::milliseconds>(stop_dynamic - start_dynamic);
                 dynamicFoundCycle = true;
@@ -104,8 +136,10 @@ int main() {
 
         auto start_static = chrono::high_resolution_clock::now();
         StaticGraph sg = StaticGraph(N);
-        for (auto edge : edges) {
-            if (!sg.insertEdge(edge)) {
+        for (auto edge : edges)
+        {
+            if (!sg.insertEdge(edge))
+            {
                 auto stop_static = chrono::high_resolution_clock::now();
                 auto duration = chrono::duration_cast<chrono::milliseconds>(stop_static - start_static);
                 staticFoundCycle = true;
@@ -115,21 +149,25 @@ int main() {
             }
         }
 
-        if (dynamicFoundCycle && staticFoundCycle) {
+        if (dynamicFoundCycle && staticFoundCycle)
+        {
             i++;
         }
 
-        if (!dynamicFoundCycle && !staticFoundCycle) {
+        if (!dynamicFoundCycle && !staticFoundCycle)
+        {
             cout << "No cycle found, regenerating graph." << endl;
             continue;
         }
 
-        if (!dynamicFoundCycle && staticFoundCycle) {
+        if (!dynamicFoundCycle && staticFoundCycle)
+        {
             cout << "ERROR: Dynamic graph method did not find the cycle formed at: " << staticCycle.first << "->" << staticCycle.second;
             break;
         }
 
-        if (!dynamicFoundCycle && staticFoundCycle) {
+        if (!dynamicFoundCycle && staticFoundCycle)
+        {
             cout << "ERROR: Static graph method did not find the cycle formed at: " << dynamicCycle.first << "->" << dynamicCycle.second;
             break;
         }
@@ -139,8 +177,9 @@ int main() {
     cout << "Number of nodes: " << N << endl;
     cout << "Number of edges: " << M << endl;
     cout << "Number of runs: " << C << endl;
-    cout << "Dynamic graph speed average: " << dynamicSum / C << endl;
-    cout << "Static graph speed average: " << staticSum / C << endl;
+    cout << "Graph generation: " << ((Gver == 1) ? "random" : "uniform") << endl;
+    cout << "Dynamic graph speed average: " << dynamicSum / C << " ms." << endl;
+    cout << "Static graph speed average: " << staticSum / C << " ms." << endl;
 
     return 0;
 }
